edea95488ca93b807fa9b483345592475de7078b
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-16 17:46:16 -0300

    [fix] make it more simple

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,26 @@
-#[cfg(feature = "ssr")]
+use std::env;
+
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Your IP is: {}",
+        req.connection_info()
+            .realip_remote_addr()
+            .unwrap_or("unknown")
+    ))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_files::Files;
-    use actix_web::*;
-    use leptos::*;
-    use leptos_actix::{generate_route_list, LeptosRoutes};
-    use showmyip::app::*;
+    let host: String = env::var("HOST").unwrap_or("localhost".to_owned());
+    let port: String = env::var("PORT").unwrap_or("8080".to_owned());
 
-    let host: String = env::var("HOST").map_or_else(|_op| "localhost".to_owned(), |f| f);
-    let port: String = env::var("PORT").map_or_else(|_op| "3000".to_owned(), |f| f);
+    println!("Server running at {host}:{port}");
 
-    let conf = get_configuration(None).await.unwrap();
-    let addr = string::format!("{host}:{port}");
-    // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(App);
-    println!("listening on http://{}", &addr);
-
-    HttpServer::new(move || {
-        let leptos_options = &conf.leptos_options;
-        let site_root = &leptos_options.site_root;
-
-        App::new()
-            // serve JS/WASM/CSS from `pkg`
-            .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            // serve other assets from the `assets` directory
-            .service(Files::new("/assets", site_root))
-            // serve the favicon from /favicon.ico
-            .service(favicon)
-            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
-            .app_data(web::Data::new(leptos_options.to_owned()))
-        //.wrap(middleware::Compress::default())
-    })
-    .bind(&addr)?
-    .run()
-    .await
-}
-
-#[cfg(feature = "ssr")]
-#[actix_web::get("favicon.ico")]
-async fn favicon(
-    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
-) -> actix_web::Result<actix_files::NamedFile> {
-    let leptos_options = leptos_options.into_inner();
-    let site_root = &leptos_options.site_root;
-    Ok(actix_files::NamedFile::open(format!(
-        "{site_root}/favicon.ico"
-    ))?)
-}
-
-#[cfg(not(any(feature = "ssr", feature = "csr")))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-    // see optional feature `csr` instead
-}
-
-#[cfg(all(not(feature = "ssr"), feature = "csr"))]
-pub fn main() {
-    // a client-side main function is required for using `trunk serve`
-    // prefer using `cargo leptos serve` instead
-    // to run: `trunk serve --open --features csr`
-    use showmyip::app::*;
-
-    console_error_panic_hook::set_once();
-
-    leptos::mount_to_body(App);
+    HttpServer::new(|| App::new().service(index))
+        .bind(format!("{host}:{port}"))?
+        .run()
+        .await
 }
